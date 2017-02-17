f6ada388ed2dec4555714158786d589ebd739b65
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-02-17 16:51:19 +0000

    Move lasso to top

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict';
 
 require('marko/node-require').install();
+require('lasso').configure(require('./lasso'));
 
 const compression = require('compression');
 const express = require('express');
-
-require('lasso').configure(require('./lasso'));
 
 const app = express();
 const port = process.env.PORT || 4000;
