0b0228b421b5ac68bd7d00089ff239972314fa23
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-02-17 17:17:12 +0000

    Handle errors

--- src/page/index.js
+++ src/page/index.js
@@ -7,33 +7,31 @@ const template = require('./template.marko');
 
 const apiUrl = (process.env.API_URL || 'http://localhost:3000') + '/api';
 
-module.exports = function (req, res, next) {
+module.exports = function (req, res) {
   const {roomId} = req.params;
   const today = new Date();
   const tomorrow = addDays(today, 1);
 
-  got(`${apiUrl}/Rooms/${roomId}`, {
-    json: true,
-    query: {
-      filter: JSON.stringify({
-        include: {
-          relation: 'bookings',
-          scope: {
-            order: 'start ASC',
-            where: {
-              and: [
-                {start: {gt: formatDate(today)}},
-                {start: {lt: formatDate(tomorrow)}}
-              ]
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  template.render({
+    apiRequest: got(`${apiUrl}/Rooms/${roomId}`, {
+      json: true,
+      query: {
+        filter: JSON.stringify({
+          include: {
+            relation: 'bookings',
+            scope: {
+              order: 'start ASC',
+              where: {
+                and: [
+                  {start: {gt: formatDate(today)}},
+                  {start: {lt: formatDate(tomorrow)}}
+                ]
+              }
             }
           }
-        }
-      })
-    }
-  })
-  .then(response => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    template.render(response.body, res);
-  })
-  .catch(next);
+        })
+      }
+    })
+  }, res);
 };
